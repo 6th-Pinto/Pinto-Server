922984476ffd9a6013d62c7fa6c15a7daaa06ec9
skmn3 <sangjin170202@naver.com>
2022-03-12 23:21:31 +0900

    📑 :: App

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const { sequelize } = require('./models');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
+
+const ForbiddenException = require('./exceptions/Forbidden');
+
+require('dotenv').config();
 
 const corsOptions = {
   origin: '*',
@@ -16,9 +21,20 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors(corsOptions));
 app.use(morgan('dev'));
 
+app.set('jwt-secret', process.env.JWT_SECRET);
+
 app.get('/', (_req, res) => {
   res.json('express-starter');
 });
+
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    console.log('Connected to Database');
+  })
+  .catch(err => {
+    console.error(err);
+  });
 
 app.listen(PORT, () => {
   console.log('Listening on', PORT);
