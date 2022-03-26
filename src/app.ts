4a76f4ecc3f38c9222668bbfadab1f86d79e55ac
Sangjin <sangjin170202@naver.com>
2022-03-26 14:27:06 +0900

    📑 :: express-loader

---
+++ src/app.ts
@@ -0,0 +1,19 @@
+import express, { Application } from 'express';
+
+import config from './config';
+import loadApp from './loaders';
+
+const startServer = async () => {
+  const app: Application = express();
+
+  await loadApp(app);
+  app.listen(config.port);
+};
+
+startServer()
+  .then(() => console.log(`Server Run on ${config.port}`))
+  .catch(e => {
+    console.error('Server Run Failed');
+    console.error(e);
+    process.exit(1);
+  });
---
+++ src/loaders/express.ts
@@ -0,0 +1,25 @@
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import express, { Application } from 'express';
+import morgan from 'morgan';
+
+import routes from '../api';
+import errorHandler from '../api/middlewares/error';
+import config from '../config';
+import { commonError } from '../constants/error';
+import ErrorResponse from '../utils/error-response';
+
+export default (app: Application): void => {
+  app.use(express.json());
+  app.use(cookieParser());
+  app.use(express.urlencoded({ extended: false }));
+  app.use(cors(config.corsOptions));
+
+  app.use(morgan(process.env.NODE_ENV === 'development' ? 'dev' : 'combined'));
+
+  app.use(config.api.prefix, routes);
+
+  app.all('*', (req, res, _next) => {
+    res.status(404).send('404');
+  });
+};
