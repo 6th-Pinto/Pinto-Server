09c6cf4aef60bcc6e9fdb26709981186f313ad44
Sangjin <sangjin170202@naver.com>
2022-04-02 13:52:02 +0900

    📑 :: UserValidation

---
+++ src/api/validation/users.ts
@@ -0,0 +1,41 @@
+import { NextFunction, Request, Response } from 'express';
+import Joi from 'joi';
+import { idValidator, pwValidator } from '@teipro/validation';
+
+import ErrorResponse from '../../utils/error-response';
+import { LoginRequestBody } from '../../types';
+
+export const userTestValidation = (req: Request, _res: Response, next: NextFunction): void => {
+  const schema = Joi.object({
+    uid: Joi.string().required().messages({
+      'any.required': `사용자 고유아이디를 입력해주세요`,
+    }),
+  });
+
+  const validationResult = schema.validate(req.query);
+
+  if (validationResult.error) {
+    throw new ErrorResponse({
+      statusCode: 400,
+      message: validationResult.error.message,
+    });
+  }
+
+  next();
+};
+
+export const userCreateValidation = (req: Request, _res: Response, next: NextFunction): void => {
+  const { userId = '', password = '' } = req.body as LoginRequestBody;
+
+  const [isIdValid, idWarningMessage] = idValidator(userId);
+  const [isPwValid, pwWarningMessage] = pwValidator(password);
+
+  if (!isIdValid || !isPwValid) {
+    throw new ErrorResponse({
+      statusCode: 400,
+      message: idWarningMessage || pwWarningMessage,
+    });
+  }
+
+  next();
+};
