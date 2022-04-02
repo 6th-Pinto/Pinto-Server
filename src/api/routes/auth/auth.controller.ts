680ef595af6f6a5f2ebb8188610b75347ea7d643
Sangjin <sangjin170202@naver.com>
2022-04-02 13:56:05 +0900

    📑 :: AuthController

--- src/api/routes/auth/auth.controller.ts
+++ src/api/routes/auth/auth.controller.ts
@@ -1,9 +1,68 @@
-import { Request, Response, NextFunction } from 'express';
+import { CookieOptions, NextFunction, Request, Response } from 'express';
+import Container from 'typedi';
 
-export const AuthTest = (_req: Request, res: Response, next: NextFunction) => {
+import { REFRESH_TOKEN_COOKIE_KEY } from '../../../constants/auth';
+import JwtHelper from '../../../helpers/jwt';
+import AuthService from '../../../services/auth';
+import { LoginRequestBody } from '../../../types';
+import { getRefreshToken } from '../../../utils/jwt';
+
+export const handleLogin = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
   try {
-    res.status(200).json('auth router test');
+    const userLoginInfo = req.body as LoginRequestBody;
+
+    const authServiceInstance = Container.get(AuthService);
+    const jwtHelper = Container.get<JwtHelper>('jwtHelper');
+
+    const { access, refresh } = await authServiceInstance.login(userLoginInfo);
+
+    const refreshTokenExpires = new Date(Date.now() + jwtHelper.getRefreshExpiresInMs());
+    const refreshTokenCookieOptions: CookieOptions = {
+      expires: refreshTokenExpires,
+      secure: false,
+      httpOnly: true,
+    };
+    res.cookie(REFRESH_TOKEN_COOKIE_KEY, refresh, refreshTokenCookieOptions);
+
+    res.status(200).json({ access });
   } catch (e) {
+    next(e);
+  }
+};
+
+export const handleLogout = (_req: Request, res: Response, next: NextFunction): void => {
+  try {
+    res.clearCookie(REFRESH_TOKEN_COOKIE_KEY);
+    res.status(200).end();
+  } catch (e) {
+    next(e);
+  }
+};
+
+export const handleRefresh = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> => {
+  try {
+    const { redirect } = req.query;
+
+    const authServiceInstance = Container.get(AuthService);
+
+    const refreshToken = getRefreshToken(req.cookies);
+    const { access } = await authServiceInstance.refreshAccessToken(refreshToken);
+
+    if (redirect) {
+      res.status(200).json({ requestAgain: true, access });
+    } else {
+      res.status(200).json({ access });
+    }
+  } catch (e) {
+    res.clearCookie(REFRESH_TOKEN_COOKIE_KEY);
     next(e);
   }
 };
