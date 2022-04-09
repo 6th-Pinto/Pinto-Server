71ef64c8c9cc94341cde4e96895543db6f73ae1c
JinAru16 <70217158+JinAru16@users.noreply.github.com>
2022-04-09 20:19:15 +0900

    home path 주석처리,
    pug에서 Favicon 무시

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -11,9 +11,9 @@ import { commonError } from '../constants/error';
 import ErrorResponse from '../utils/error-response';
 
 export default (app: Application): void => {
-  app.set('view engine', 'pug');
-  app.set('views', path.join(__dirname, '/../views/'));
-  app.use('/public', express.static(path.join(__dirname, '/../public')));
+//  app.set('view engine', 'pug');
+// app.set('views', path.join(__dirname, '/../views/'));
+//  app.use('/public', express.static(path.join(__dirname, '/../public')));
 //  app.get('/', (req, res) => res.render('home'));
   app.use(express.json());
   app.use(cookieParser());
