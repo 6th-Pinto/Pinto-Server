21fa0853075e0c2457b00093c4fe26f1a09d7cd6
JinAru16 <70217158+JinAru16@users.noreply.github.com>
2022-04-09 20:14:15 +0900

    home path 주석처리

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -14,7 +14,7 @@ export default (app: Application): void => {
   app.set('view engine', 'pug');
   app.set('views', path.join(__dirname, '/../views/'));
   app.use('/public', express.static(path.join(__dirname, '/../public')));
-  app.get('/', (req, res) => res.render('home'));
+//  app.get('/', (req, res) => res.render('home'));
   app.use(express.json());
   app.use(cookieParser());
   app.use(express.urlencoded({ extended: false }));
