03a618033db2536f38fdc305582a7ca5946c86df
JinAru16 <70217158+JinAru16@users.noreply.github.com>
2022-04-10 14:01:37 +0900

    block router users

--- src/api/routes/users/index.ts
+++ src/api/routes/users/index.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 
 import { userCreateValidation, userTestValidation } from '../../validation/users';
-import { handleCreateUser, handleUserTest } from './users.controller';
+// import { handleCreateUser, handleUserTest } from './users.controller';
 
 const router = Router();
 
-// outer.get('/', userTestValidation, handleUserTest);
-router.post('/', userCreateValidation, handleCreateUser);
+// router.get('/', userTestValidation, handleUserTest);
+router.post('/', userCreateValidation) // handleCreateUser);
 
 export default router;
