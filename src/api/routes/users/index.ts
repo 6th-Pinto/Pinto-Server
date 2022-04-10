53ab8964f8a9f6820b506aa040e3d21e47492573
JinAru16 <70217158+JinAru16@users.noreply.github.com>
2022-04-10 13:50:51 +0900

    block all get method

--- src/api/routes/users/index.ts
+++ src/api/routes/users/index.ts
@@ -5,7 +5,7 @@ import { handleCreateUser, handleUserTest } from './users.controller';
 
 const router = Router();
 
-router.get('/', userTestValidation, handleUserTest);
+// outer.get('/', userTestValidation, handleUserTest);
 router.post('/', userCreateValidation, handleCreateUser);
 
 export default router;
